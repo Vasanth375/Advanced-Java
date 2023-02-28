359dcec9067218247bbfbaf8e3b9a48a5e6740b7
Vasanth N <vasanthnimmala69@outlook.com>
2023-02-28 22:55:46 +0530

    Country Flags

--- Java/Programs/CountryFlags.java
+++ Java/Programs/CountryFlags.java
@@ -68,18 +68,18 @@ public class CountryFlags {
         final JTextField tf=new JTextField();  
         tf.setBounds(50,50, 150,20);  
 
-        JButton b=new JButton("Click Here");  
-        b.setBounds(50,100,95,30);  
+        JButton b=new JButton(new ImageIcon("C:\\Users\\VASANTH\\OneDrive\\India-Flag-PNG-Free-Download.png"));  
+        b.setBounds(50,100,100,100);  
 
         b.addActionListener(new ActionListener(){  
         public void actionPerformed(ActionEvent e){  
-                tf.setText("Welcome to Javatpoint.");  
+                tf.setText("India");  
             }  
         });  
 
         f.add(b);
         f.add(tf);  
-        
+
         f.setSize(400,400);  
         f.setLayout(null);  
         f.setVisible(true);   
